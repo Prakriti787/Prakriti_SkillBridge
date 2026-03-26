a021bbca44c65720f137694ab6d6a12c43a8bcf6
Prakriti Katwal <prakritikatwal077@gmail.com>
2026-03-27 02:35:08 +0545

    3rd commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const pk=[10,20,30]
+const user='prakriti';
+export default user
---
+++ script.jsx
@@ -0,0 +1,9 @@
+// var h1= document.createElement('h1')
+
+// h1.innerHTML='Hello from js'
+// document.body.appendChild(h1)
+
+// import username from './app.js'
+// console.log(username);
+
+var h1= <h1>Hello</h1>
